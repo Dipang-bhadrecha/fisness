7ee6a9c7c06e60d5ca96168083e847962d5bf449
Dipang-bhadrecha <dipaang.bhadrecha@gmail.com>
2026-03-08 19:06:03 +0530

    asset folder fixed

--- constants/fishImages.ts
+++ constants/fishImages.ts
@@ -1,7 +1,7 @@
 /**
  * Fish Image Map
  *
- * Each fish ID maps to its image in assets/images/fish/
+ * Each fish ID maps to its image in assets/fish/
  * You already have these files in your repo ✅
  *
  * BACKEND READY: When API is connected, swap any require()
@@ -11,19 +11,19 @@
 type FishImageSource = ReturnType<typeof require> | { uri: string } | null
 
 const FISH_IMAGES: Record<string, FishImageSource> = {
-  jumbo:    require('../assets/images/fish/jumbo.jpg'),
-  pomfret:  require('../assets/images/fish/promphet.jpg'),   
-  bhungar:  require('../assets/images/fish/bhungar.jpg'),
-  narsinga: require('../assets/images/fish/narsinga.jpg'),
-  white:    require('../assets/images/fish/white.jpg'),
-  prati:    require('../assets/images/fish/prati.jpg'),
-  squid:    require('../assets/images/fish/squid.jpg'),
-  crab:     require('../assets/images/fish/crab.jpg'),
-  prawns_l: require('../assets/images/fish/prawns_l.jpg'),
-  prawns_s: require('../assets/images/fish/prawns_s.jpg'),
-  kolami:   require('../assets/images/fish/kolami.jpg'),
-  tiger:    require('../assets/images/fish/tiger.jpg'),
-  flowers:  require('../assets/images/fish/flowers.jpg'),
+  jumbo:    require('../assets/fish/jumbo.jpg'),
+  pomfret:  require('../assets/fish/promphet.jpg'),
+  bhungar:  require('../assets/fish/bhungar.jpg'),
+  narsinga: require('../assets/fish/narsinga.jpg'),
+  white:    require('../assets/fish/white.jpg'),
+  prati:    require('../assets/fish/prati.jpg'),
+  squid:    require('../assets/fish/squid.jpg'),
+  crab:     require('../assets/fish/crab.jpg'),
+  prawns_l: require('../assets/fish/prawns_l.jpg'),
+  prawns_s: require('../assets/fish/prawns_s.jpg'),
+  kolami:   require('../assets/fish/kolami.jpg'),
+  tiger:    require('../assets/fish/tiger.jpg'),
+  flowers:  require('../assets/fish/flowers.jpg'),
 }
 
 /**
